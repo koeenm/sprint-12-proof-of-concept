a3c5641fc204a7d9b2faf5958d7f45b95986ce6c
SamaraFellaDina <sammybensink@gmail.com>
2024-06-04 22:52:15 +0200

    opzetten van de server om in te bouwen en stijlen #3

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('Hier komt je server voor Sprint 12.')
+// importeren
+import express from "express"
+import fetchJson from './helpers/fetch-json.js'
+const app = express()
+
+// stel de mappen in
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+app.set('port', process.env.PORT || 6090)
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+app.get('/', (request, response) => {
+  response.render('index.ejs')
+})
+
+
