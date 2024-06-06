07a8636db0a2c890b6d42405b2d4ddf7dbd85fcd
SamaraFellaDina <sammybensink@gmail.com>
2024-06-06 15:49:40 +0200

    aanroepen van de analytics #19

--- server.js
+++ server.js
@@ -2,6 +2,47 @@ import express from "express"
 import fetchJson from './helpers/fetch-json.js'
 const app = express()
 
+// Importeren van geheime data
+import 'dotenv/config'
+console.log(`Hello ${process.env.GoogleData}`)
+
+
+function main(propertyId = '301922918') {
+  const {BetaAnalyticsDataClient} = require('@google-analytics/data');
+  const analyticsDataClient = new BetaAnalyticsDataClient();
+
+
+  async function runReport() {
+    const [response] = await analyticsDataClient.runReport({
+      property: `properties/${propertyId}`,
+      dateRanges: [
+        {
+          startDate: '2020-03-31',
+          endDate: 'today',
+        },
+      ],
+      dimensions: [
+        {
+          name: 'city',
+        },
+      ],
+      metrics: [
+        {
+          name: 'activeUsers',
+        },
+      ],
+    });
+
+    console.log('Report result:');
+    response.rows.forEach((row) => {
+      console.log(row.dimensionValues[0], row.metricValues[0]);
+    });
+  }
+  runReport();
+}
+
+
+
 app.set('view engine', 'ejs')
 app.set('views', './views')
 app.use(express.static('public'))
