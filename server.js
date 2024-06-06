84c7bc01e6a09440af3207bdef15c8c058163409
SamaraFellaDina <sammybensink@gmail.com>
2024-06-06 13:46:26 +0200

    klaarzetten van de server

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-// importeren
 import express from "express"
 import fetchJson from './helpers/fetch-json.js'
 const app = express()
 
-// stel de mappen in
 app.set('view engine', 'ejs')
 app.set('views', './views')
 app.use(express.static('public'))
