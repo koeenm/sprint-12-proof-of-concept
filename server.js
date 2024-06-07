eb5f047a07a3693ea7b5adb02fe8a3195b6dabbc
koeenm <mkoeen2@gmail.com>
2024-06-07 11:32:43 +0200

    eerste deel

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express'
+import fetchJson from './helpers/fetch-json.js'
+
+const app = express()
+const fdr_site = ('https://fdnd-agency.directus.app/items/frd_site')
+const frd_scans = ('https://fdnd-agency.directus.app/items/frd_scans')
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (request, response) => {
+    response.render('index')
+})
+
+app.set('port', process.env.PORT || 8080)
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
