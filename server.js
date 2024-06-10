81922bf0c030c4cb0e117dcac7e067241bdf2687
SamaraFellaDina <sammybensink@gmail.com>
2024-06-10 13:14:54 +0200

    opzetten van de API #19fix

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express"
 import fetchJson from './helpers/fetch-json.js'
-const app = express()
 
-// Importeren van geheime data
-import 'dotenv/config'
+const propertyId = '301922918';
+import {BetaAnalyticsDataClient} from '@google-analytics/data';
+const analyticsDataClient = new BetaAnalyticsDataClient();
+
+const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
@@ -15,8 +17,27 @@ app.listen(app.get('port'), function () {
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
 
-app.get('/', (request, response) => {
-  response.render('home.ejs')
+app.get('/', async (request, response) => {
+  const [apiDataCountries] = await analyticsDataClient.runReport({
+    property: `properties/${propertyId}`,
+    dateRanges: [
+      {
+        startDate: '2024-03-31',
+        endDate: 'today',
+      },
+    ],
+    dimensions: [
+      {
+        name: 'country',
+      },
+    ],
+    metrics: [
+      {
+        name: 'activeUsers',
+      },
+    ],
+  });
+  response.render('home.ejs', {data: apiDataCountries})
 })
 
 
